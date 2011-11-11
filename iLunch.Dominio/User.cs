848d5e1733a30cd01574e292844fce1aa4cd7e3a
Joao Paulo Leonidas <jpaulo@auctor.com.br>
2011-11-10 21:22:55 -0300

    Só um teste.

--- iLunch.Dominio/User.cs
+++ iLunch.Dominio/User.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace iLunch.Dominio
 {
+    /// <summary>
+    /// User class.
+    /// </summary>
     public class User : Entity
     {
         public virtual string Name { get; set; }
